394517af7402b523924460479337c08b5e35308b
Miguel <mgarcia95951@gmail.com>
2019-04-02 13:46:44 -0500

    added sample pages

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 class About extends React.Component {
   render() {
     return (
       <div className="App about">
         <h1>About</h1>
-        <Link to='/about'>
+        <Link to='/'>
           <h2>Go To Home</h2>
         </Link>
       </div>
