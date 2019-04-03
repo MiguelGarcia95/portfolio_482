cc0d6fee93764a2aa4e69df6fff1a2620a11b9a3
Miguel <mgarcia95951@gmail.com>
2019-04-02 22:23:06 -0500

    work on page transitions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Switch, BrowserRouter, Route} from 'react-router-dom';
+import barba from '@barba/core'; // Or nothing if loaded via the browser
 
 import Home from './components/pages/Home';
 import About from './components/pages/About';
