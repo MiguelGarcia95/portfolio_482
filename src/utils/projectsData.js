cf9e47aad8a9df38d21fbc1290dc360647f11f67
Miguel <mgarcia95951@gmail.com>
2019-04-04 02:20:27 -0500

    work on page transitions

---
+++ src/utils/projectsData.js
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    name: 'testing-data',
+    imageUrl: 'testing-data.png',
+    about: 'testing data goes here...',
+    details: 'noooo',
+    tags: [
+      'anime', 'gaming', 'random'
+    ]
+  },
+  {
+    name: 'testing-data2',
+    imageUrl: 'testing-data2.png',
+    about: 'testing data goes here...',
+    details: 'noooo',
+    tags: [
+      'anime', 'gaming', 'random'
+    ]
+  },
+]
