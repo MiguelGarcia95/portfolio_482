8bc514624d4fed821ef37c4834edfeb24613530e
Miguel <mgarcia95951@gmail.com>
2019-04-05 05:26:44 -0500

    set up work page

--- src/components/layout/WorkItem.jsx
+++ src/components/layout/WorkItem.jsx
@@ -11,9 +11,12 @@ const WorkItem = ({project, unloadContent}) => {
   return (
     <section className="work_item">
       <section className="work_item_image" style={imageStyle} ></section>
-      <Link className='noselect' to='/work/testing-data' onClick={() => {unloadContent()}}>
-        <h3>Go 1</h3>
-      </Link>
+      <section className="work_item_details">
+        <h2 className="work_item_name">{project.name}</h2>
+        <Link className='noselect' to='/work/testing-data' onClick={() => {unloadContent()}}>
+          <h3>Go 1</h3>
+        </Link>
+      </section>
     </section>
   )
 }
