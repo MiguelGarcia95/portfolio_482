af0c129fe678fafe9b2fda81c4f4e01f645f63b0
Miguel <mgarcia95951@gmail.com>
2019-04-05 05:45:27 -0500

    set up work page

--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -26,4 +26,49 @@ export const projects = [
       'anime', 'gaming', 'random'
     ]
   },
+  {
+    name: 'testing-data4',
+    imageUrl: 'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
+    about: 'testing data goes here...',
+    details: 'noooo',
+    tags: [
+      'anime', 'gaming', 'random'
+    ]
+  },
+  {
+    name: 'testing-data5',
+    imageUrl: 'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
+    about: 'testing data goes here...',
+    details: 'noooo',
+    tags: [
+      'anime', 'gaming', 'random'
+    ]
+  },
+  {
+    name: 'testing-data6',
+    imageUrl: 'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
+    about: 'testing data goes here...',
+    details: 'noooo',
+    tags: [
+      'anime', 'gaming', 'random'
+    ]
+  },
+  {
+    name: 'testing-data7',
+    imageUrl: 'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
+    about: 'testing data goes here...',
+    details: 'noooo',
+    tags: [
+      'anime', 'gaming', 'random'
+    ]
+  },
+  {
+    name: 'testing-data8',
+    imageUrl: 'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
+    about: 'testing data goes here...',
+    details: 'noooo',
+    tags: [
+      'anime', 'gaming', 'random'
+    ]
+  },
 ]
