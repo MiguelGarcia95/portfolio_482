ce4659e11a38f38991c0fec89c046c597947b6ec
Miguel <mgarcia95951@gmail.com>
2019-04-06 00:27:31 -0500

    set up work page

--- src/components/layout/ProjectSlider.jsx
+++ src/components/layout/ProjectSlider.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from "react-slick";
 import ProjectSliderSlide from './ProjectSliderSlide';
 
-displaySlides = slides => {
+const displaySlides = slides => {
   return slides.map((slide, index) => {
     return <ProjectSliderSlide key={index} />
   })
