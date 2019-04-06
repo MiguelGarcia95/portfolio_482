cc74d7ab0094d69483cdeb18352b88ef865d2a80
Miguel <mgarcia95951@gmail.com>
2019-04-06 16:46:06 -0500

    set up work page

--- src/components/layout/ProjectSliderSlide.jsx
+++ src/components/layout/ProjectSliderSlide.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ProjectSliderSlide = ({color}) => {
   return (
     <section className='project_image_slide' style={{backgroundColor: color}}>
-    <h1>ss</h1>
+      {/* <h1>ss</h1> */}
     </section>
   );
 }
