559f2bfdd602c37e4092ff1f23ae88625cd32a3c
Miguel <mgarcia95951@gmail.com>
2019-04-08 14:20:01 -0500

    set up work page

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 import './css/Navbar.css';
 
 const Navbar = () => {
   return (
     <section className="nav_menu">
-      <Link to='/' className='noselect' ><p>Home</p></Link>
-      <Link to='/work' className='noselect' ><p>Work</p></Link>
-      <Link to='/contact' className='noselect' ><p>Contact</p></Link>          
+      <NavLink activeClassName="active" exact to='/' className='noselect' ><p>Home</p></NavLink>
+      <NavLink activeClassName="active" to='/work' className='noselect' ><p>Work</p></NavLink>
+      <NavLink activeClassName="active" to='/contact' className='noselect' ><p>Contact</p></NavLink>          
     </section>
   )
 }
