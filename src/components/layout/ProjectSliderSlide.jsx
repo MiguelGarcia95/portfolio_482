2be39d075e1ae506ad0c435a8ba4bb7ed55e3ae6
Miguel <mgarcia95951@gmail.com>
2019-04-08 09:24:33 -0500

    set up work page

--- src/components/layout/ProjectSliderSlide.jsx
+++ src/components/layout/ProjectSliderSlide.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const ProjectSliderSlide = ({image}) => {
-  
+  let imageStyle = {
+    backgroundImage: `url(${image})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'
+  }
   return (
-    <section className='project_image_slide' style={{backgroundColor: color}}>
+    <section className='project_image_slide' style={imageStyle}>
     </section>
   );
 }
