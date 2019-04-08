622dd70651bd0ea854f70593795c6e7b11336a29
Miguel <mgarcia95951@gmail.com>
2019-04-08 14:11:14 -0500

    set up work page

--- src/components/pages/ShowWork.jsx
+++ src/components/pages/ShowWork.jsx
@@ -77,6 +77,7 @@ class ShowWork extends React.Component {
             <section className="show_work_display">
               <section className="show_work_display_header">
                 <h1 className="show_work_name">{project.name}</h1>
+                <a href="linkhere.com" className="show_work_link">Visit Page</a>
               </section>
               <ProjectSlider images={project.images} />
             </section>
