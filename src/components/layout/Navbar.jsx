e98af7a7941ba40f74fb923c29563b457c9f7e68
Miguel <mgarcia95951@gmail.com>
2019-04-07 19:58:40 -0500

    set up work page

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,9 +4,9 @@ import {Link} from 'react-router-dom';
 const Navbar = () => {
   return (
     <section className="nav_menu">
-      <Link to='/'><p>Home</p></Link>
-      <Link to='work'><p>Work</p></Link>
-      <Link to='contact'><p>Contact</p></Link>          
+      <Link to='/' className='noselect' ><p>Home</p></Link>
+      <Link to='work' className='noselect' ><p>Work</p></Link>
+      <Link to='contact' className='noselect'  ><p>Contact</p></Link>          
     </section>
   )
 }
