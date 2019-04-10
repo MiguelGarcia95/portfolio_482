f16b2acf453baa9c3b3191c97ade2eeb2b039104
Miguel <mgarcia95951@gmail.com>
2019-04-10 14:09:45 -0500

    create home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import styled, { keyframes } from "styled-components";
+// import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 import {Page} from '../../utils/styledClasses';
 import {slideInLeft, slideOutLeft} from '../../utils/styledTransitions';
