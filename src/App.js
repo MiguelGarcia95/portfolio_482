9bf9cd488c4791712fcc8f453738b7f0683084d1
Miguel <mgarcia95951@gmail.com>
2019-04-10 19:14:23 -0500

    create home page

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
             <PageContainer>
               <TransitionGroup component={null}>
                 <CSSTransition
-                  timeout={500}
+                  timeout={800}
                   classNames='page'
                   key={location.key}
                 >
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-// import styled, { keyframes } from "styled-components";
 import styled from "styled-components";
 
 import {Page} from '../../utils/styledClasses';
+import {loadContent, unloadContent} from '../../utils/functions';
 import {slideInLeft, slideOutLeft} from '../../utils/styledTransitions';
 import Navbar from '../layout/Navbar';
 import './css/Home.css';
@@ -17,22 +17,47 @@ const HomePageElm = styled(Page)`
   }
 `;
 
-const Home = () => {
-  return (
-    <HomePageElm className='app home'>
-      <Navbar />
-      <section className="home_container">
-        <section className="home_header">
-          <h1>Miguel Garcia</h1>
+class Home extends React.Component {
+  componentDidMount() {
+    setInterval(() => {
+      loadContent('.home_container', '90vh');
+    }, 500);
+  }
+
+  compoenntWillUnmount() {
+    setInterval(() => {
+      unloadContent('.home_container');
+    }, 500);
+  }
+
+//   Hey! I’m Miguel, a Web Developer with an ever growing passion for frontend development , and some interest in backend technologies. Web Apps are the future after all.
+// Preferred web technologies to work with include:
+
+//     CSS
+//     Javascript (ReactJS)
+//     any Javascript animation library (GSAP, and Three.JS in particular),
+//     PHP (Laravel)
+//     Wordpress (seeing what some plugins do is something else).
+
+// Personal hobbies include reading, art/design, video games, and occasionally lurk blogs and forums.
+
+  render() {
+    return (
+      <HomePageElm className='app home'>
+        <Navbar />
+        <section className="home_container">
+          <section className="home_header">
+            <h1>Miguel Garcia</h1>
+          </section>
+          <section className="home_button">
+            <Link to='/work'>
+              <i className="fas fa-long-arrow-alt-right fa-2x"></i>
+            </Link>
+          </section>
         </section>
-        <section className="home_button">
-          <Link to='/work'>
-            <i className="fas fa-long-arrow-alt-right fa-2x"></i>
-          </Link>
-        </section>
-      </section>
-    </HomePageElm>
-  )
+      </HomePageElm>
+    )
+  }
 }
 
 export default Home;
