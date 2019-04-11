be24b339a02d293e60ee9dcb4fcd59757c11e04f
Miguel <mgarcia95951@gmail.com>
2019-04-11 14:03:19 -0500

    add work page details

---
+++ src/components/layout/WorkSlider.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const WorkSlider = ({project}) => {
+  return (
+    <section className="show_work_display">
+      <section className="show_work_display_header">
+        <h1 className="show_work_name">{project.name}</h1>
+        <a href="linkhere.com" className="show_work_link">Visit Page</a>
+      </section>
+      <ProjectSlider images={project.images} />
+    </section>
+  )
+}
+
+export default WorkSlider;
