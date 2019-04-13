00d47377b6b5c22f75da683be0733d2b5d8aca57
Miguel <mgarcia95951@gmail.com>
2019-04-13 17:13:40 -0500

    add project data

--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -51,12 +51,12 @@ export const projects = [
       'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png'
     ],
     about: 'testing data goes here...',
-    details: "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn. S'uhn kadishtuyar Tsathoggua, y-hupadgh syha'hor f'sll'ha throd ch' li'hee nglui, stell'bsna vulgtlagln ilyaa Nyarlathotep zhro shagg 'aiog. Gotha nog gof'nn ah ya Dagon 'ai f'ron n'gha ch' orr'e f'gof'nn, hlirghog y'hah ngluioth gothaagl R'lyeh lw'nafh athg Azathothog ftaghu mnahn' vulgtlagln, Azathoth shagg n'gha uln kn'a li'hee athgyar naHastur nilgh'ri ngYoggoth. Azathoth r'luh shtunggli phlegeth hrii y'hah bug nog, bug geb gof'nn Azathoth gof'nn nnnDagon vulgtlagln, h'orr'e tharanakyar 'ai zhro k'yarnak Dagon. Sgn'wahl gnaiih ep phlegeth wgah'n uh'e Yoggothnyth lloigoth, fm'latgh cy'hah mnahn' ngzhro f'k'yarnak lloig, nafm'latgh 'ai tharanak f'Dagon Tsathoggua shtunggli. ",
+    details: "A Wordpress website made for a local roofing company. The client wanted his website rebuilt since he wasn't satisfied with his initial website. I made the design, and after approval made the website using Wordpress and the Elementor plugin.",
     tags: [
       'anime', 'gaming', 'random', 'anime', 'gaming', 'random', 'anime', 'gaming', 'random'
     ],
     technology: [
-      'HTML', 'CSS3 - LESS', 'Wordpress'
+      'HTML', 'CSS3 - LESS', 'Wordpress', 'Elementor'
     ]
   },
   {
