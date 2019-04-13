ca626ae59243d5eb18ea34905e188d9847740271
Miguel <mgarcia95951@gmail.com>
2019-04-13 17:50:18 -0500

    add project data

--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -87,7 +87,8 @@ export const projects = [
       'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
       'https://s3.amazonaws.com/crowdspring3-assets/marketing/crowdspring_marketing_faq_artwork.png'
     ],
-    details: "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn. S'uhn kadishtuyar Tsathoggua, y-hupadgh syha'hor f'sll'ha throd ch' li'hee nglui, stell'bsna vulgtlagln ilyaa Nyarlathotep zhro shagg 'aiog. Gotha nog gof'nn ah ya Dagon 'ai f'ron n'gha ch' orr'e f'gof'nn, hlirghog y'hah ngluioth gothaagl R'lyeh lw'nafh athg Azathothog ftaghu mnahn' vulgtlagln, Azathoth shagg n'gha uln kn'a li'hee athgyar naHastur nilgh'ri ngYoggoth. Azathoth r'luh shtunggli phlegeth hrii y'hah bug nog, bug geb gof'nn Azathoth gof'nn nnnDagon vulgtlagln, h'orr'e tharanakyar 'ai zhro k'yarnak Dagon. Sgn'wahl gnaiih ep phlegeth wgah'n uh'e Yoggothnyth lloigoth, fm'latgh cy'hah mnahn' ngzhro f'k'yarnak lloig, nafm'latgh 'ai tharanak f'Dagon Tsathoggua shtunggli. ",
+    details: "I recently started playing one of the newer games, and got the idea to make an online Pokedex for personal use. Once I found out there was a Pokemon API, I got started on building this Pokedex application. It was built using ReactJS, and the PokeAPI Api. It was also good practice on advanced CSS selectors.",
+    details2: "For the design, I used the first generation Kanto Pokedex design from the Pokemon anime.",
     tags: [
       'anime', 'gaming', 'random', 'anime', 'gaming', 'random', 'anime', 'gaming', 'random'
     ],
