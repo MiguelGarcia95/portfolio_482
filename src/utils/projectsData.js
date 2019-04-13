39530a95851d91b7de0ea026d48bec59c9553638
Miguel <mgarcia95951@gmail.com>
2019-04-13 14:43:28 -0500

    add project data

--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -68,7 +68,8 @@ export const projects = [
     ]
   },
   {
-    name: 'testing-data5',
+    name: 'Pokedex React App',
+    url: 'https://miguelgarcia95.github.io/PokedexApp/',
     previewImage: 'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
     imageUrl: 'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
     images: [
@@ -83,53 +84,5 @@ export const projects = [
     tags: [
       'anime', 'gaming', 'random', 'anime', 'gaming', 'random', 'anime', 'gaming', 'random'
     ]
-  },
-  {
-    name: 'testing-data6',
-    previewImage: 'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-    imageUrl: 'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-    images: [
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png'
-    ],
-    about: 'testing data goes here...',
-    details: "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn. S'uhn kadishtuyar Tsathoggua, y-hupadgh syha'hor f'sll'ha throd ch' li'hee nglui, stell'bsna vulgtlagln ilyaa Nyarlathotep zhro shagg 'aiog. Gotha nog gof'nn ah ya Dagon 'ai f'ron n'gha ch' orr'e f'gof'nn, hlirghog y'hah ngluioth gothaagl R'lyeh lw'nafh athg Azathothog ftaghu mnahn' vulgtlagln, Azathoth shagg n'gha uln kn'a li'hee athgyar naHastur nilgh'ri ngYoggoth. Azathoth r'luh shtunggli phlegeth hrii y'hah bug nog, bug geb gof'nn Azathoth gof'nn nnnDagon vulgtlagln, h'orr'e tharanakyar 'ai zhro k'yarnak Dagon. Sgn'wahl gnaiih ep phlegeth wgah'n uh'e Yoggothnyth lloigoth, fm'latgh cy'hah mnahn' ngzhro f'k'yarnak lloig, nafm'latgh 'ai tharanak f'Dagon Tsathoggua shtunggli. ",
-    tags: [
-      'anime', 'gaming', 'random', 'anime', 'gaming', 'random', 'anime', 'gaming', 'random'
-    ]
-  },
-  {
-    name: 'testing-data7',
-    previewImage: 'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-    imageUrl: 'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-    images: [
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png'
-    ],
-    about: 'testing data goes here...',
-    details: "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn. S'uhn kadishtuyar Tsathoggua, y-hupadgh syha'hor f'sll'ha throd ch' li'hee nglui, stell'bsna vulgtlagln ilyaa Nyarlathotep zhro shagg 'aiog. Gotha nog gof'nn ah ya Dagon 'ai f'ron n'gha ch' orr'e f'gof'nn, hlirghog y'hah ngluioth gothaagl R'lyeh lw'nafh athg Azathothog ftaghu mnahn' vulgtlagln, Azathoth shagg n'gha uln kn'a li'hee athgyar naHastur nilgh'ri ngYoggoth. Azathoth r'luh shtunggli phlegeth hrii y'hah bug nog, bug geb gof'nn Azathoth gof'nn nnnDagon vulgtlagln, h'orr'e tharanakyar 'ai zhro k'yarnak Dagon. Sgn'wahl gnaiih ep phlegeth wgah'n uh'e Yoggothnyth lloigoth, fm'latgh cy'hah mnahn' ngzhro f'k'yarnak lloig, nafm'latgh 'ai tharanak f'Dagon Tsathoggua shtunggli. ",
-    tags: [
-      'anime', 'gaming', 'random', 'anime', 'gaming', 'random', 'anime', 'gaming', 'random'
-    ]
-  },
-  {
-    name: 'testing-data8',
-    previewImage: 'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-    imageUrl: 'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-    images: [
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png'
-    ],
-    about: 'testing data goes here...',
-    details: "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn. S'uhn kadishtuyar Tsathoggua, y-hupadgh syha'hor f'sll'ha throd ch' li'hee nglui, stell'bsna vulgtlagln ilyaa Nyarlathotep zhro shagg 'aiog. Gotha nog gof'nn ah ya Dagon 'ai f'ron n'gha ch' orr'e f'gof'nn, hlirghog y'hah ngluioth gothaagl R'lyeh lw'nafh athg Azathothog ftaghu mnahn' vulgtlagln, Azathoth shagg n'gha uln kn'a li'hee athgyar naHastur nilgh'ri ngYoggoth. Azathoth r'luh shtunggli phlegeth hrii y'hah bug nog, bug geb gof'nn Azathoth gof'nn nnnDagon vulgtlagln, h'orr'e tharanakyar 'ai zhro k'yarnak Dagon. Sgn'wahl gnaiih ep phlegeth wgah'n uh'e Yoggothnyth lloigoth, fm'latgh cy'hah mnahn' ngzhro f'k'yarnak lloig, nafm'latgh 'ai tharanak f'Dagon Tsathoggua shtunggli. ",
-    tags: [
-      'anime', 'gaming', 'random', 'anime', 'gaming', 'random', 'anime', 'gaming', 'random'
-    ]
-  },
+  }
 ]
