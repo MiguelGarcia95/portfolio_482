74dd98c29cf300ea1087a127c306b3e1c62e0695
Miguel <mgarcia95951@gmail.com>
2019-04-14 12:45:32 -0500

    add project data

--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -55,9 +55,9 @@ export const projects = [
     url: 'https://miguelgarcia95.github.io/coffee-place/',
     previewImage: '/images/previews/coffee.png',
     images: [
-      '/images/projects/coffee_01.png',
-      '/images/projects/coffee_02.png',
-      '/images/projects/coffee_03.png'
+      '/images/projects/coffee-01.png',
+      '/images/projects/coffee-02.png',
+      '/images/projects/coffee-03.png'
     ],
     details: "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn. S'uhn kadishtuyar Tsathoggua, y-hupadgh syha'hor f'sll'ha throd ch' li'hee nglui, stell'bsna vulgtlagln ilyaa Nyarlathotep zhro shagg 'aiog. Gotha nog gof'nn ah ya Dagon 'ai f'ron n'gha ch' orr'e f'gof'nn, hlirghog y'hah ngluioth gothaagl R'lyeh lw'nafh athg Azathothog ftaghu mnahn' vulgtlagln, Azathoth shagg n'gha uln kn'a li'hee athgyar naHastur nilgh'ri ngYoggoth. Azathoth r'luh shtunggli phlegeth hrii y'hah bug nog, bug geb gof'nn Azathoth gof'nn nnnDagon vulgtlagln, h'orr'e tharanakyar 'ai zhro k'yarnak Dagon. Sgn'wahl gnaiih ep phlegeth wgah'n uh'e Yoggothnyth lloigoth, fm'latgh cy'hah mnahn' ngzhro f'k'yarnak lloig, nafm'latgh 'ai tharanak f'Dagon Tsathoggua shtunggli. ",
     tags: [
