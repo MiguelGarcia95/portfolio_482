811c2e1d15d8e19af04854cf0f697a4213682841
Miguel <mgarcia95951@gmail.com>
2019-04-14 01:51:20 -0500

    add project images

--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -23,10 +23,9 @@ export const projects = [
     previewImage: 'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
     imageUrl: 'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
     images: [
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png'
+      '/images/projects/chat_01.png',
+      '/images/projects/chat_02.png',
+      '/images/projects/chat_03.png'
     ],
     details: "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn. S'uhn kadishtuyar Tsathoggua, y-hupadgh syha'hor f'sll'ha throd ch' li'hee nglui, stell'bsna vulgtlagln ilyaa Nyarlathotep zhro shagg 'aiog. Gotha nog gof'nn ah ya Dagon 'ai f'ron n'gha ch' orr'e f'gof'nn, hlirghog y'hah ngluioth gothaagl R'lyeh lw'nafh athg Azathothog ftaghu mnahn' vulgtlagln, Azathoth shagg n'gha uln kn'a li'hee athgyar naHastur nilgh'ri ngYoggoth. Azathoth r'luh shtunggli phlegeth hrii y'hah bug nog, bug geb gof'nn Azathoth gof'nn nnnDagon vulgtlagln, h'orr'e tharanakyar 'ai zhro k'yarnak Dagon. Sgn'wahl gnaiih ep phlegeth wgah'n uh'e Yoggothnyth lloigoth, fm'latgh cy'hah mnahn' ngzhro f'k'yarnak lloig, nafm'latgh 'ai tharanak f'Dagon Tsathoggua shtunggli. ",
     tags: [
