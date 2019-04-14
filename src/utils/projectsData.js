e3ddc92296e4a78a61a25ff8e7bb99e245ba2e12
Miguel <mgarcia95951@gmail.com>
2019-04-14 01:52:50 -0500

    add project images

--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -59,10 +59,9 @@ export const projects = [
     previewImage: 'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
     imageUrl: 'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
     images: [
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png'
+      '/images/projects/coffee_01.png',
+      '/images/projects/coffee_02.png',
+      '/images/projects/coffee_03.png'
     ],
     details: "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn. S'uhn kadishtuyar Tsathoggua, y-hupadgh syha'hor f'sll'ha throd ch' li'hee nglui, stell'bsna vulgtlagln ilyaa Nyarlathotep zhro shagg 'aiog. Gotha nog gof'nn ah ya Dagon 'ai f'ron n'gha ch' orr'e f'gof'nn, hlirghog y'hah ngluioth gothaagl R'lyeh lw'nafh athg Azathothog ftaghu mnahn' vulgtlagln, Azathoth shagg n'gha uln kn'a li'hee athgyar naHastur nilgh'ri ngYoggoth. Azathoth r'luh shtunggli phlegeth hrii y'hah bug nog, bug geb gof'nn Azathoth gof'nn nnnDagon vulgtlagln, h'orr'e tharanakyar 'ai zhro k'yarnak Dagon. Sgn'wahl gnaiih ep phlegeth wgah'n uh'e Yoggothnyth lloigoth, fm'latgh cy'hah mnahn' ngzhro f'k'yarnak lloig, nafm'latgh 'ai tharanak f'Dagon Tsathoggua shtunggli. ",
     tags: [
@@ -78,11 +77,7 @@ export const projects = [
     previewImage: 'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
     imageUrl: 'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
     images: [
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-      'https://docs.microsoft.com/en-us/sharepoint/dev/images/design-placeholders-and-fallbacks.png',
-      'https://s3.amazonaws.com/crowdspring3-assets/marketing/crowdspring_marketing_faq_artwork.png'
+      '/images/projects/pk1.png'
     ],
     details: "I recently started playing one of the newer games, and got the idea to make an online Pokedex for personal use. Once I found out there was a Pokemon API, I got started on building this Pokedex application. It was built using ReactJS, and the PokeAPI Api. It was also good practice on advanced CSS selectors.",
     details2: "For the design, I used the first generation Kanto Pokedex design from the Pokemon anime.",
